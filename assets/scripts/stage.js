64dd16ffafd890dbc27a4a3450d20d245783555f
stmaccarelli <reddo.theking@gmail.com>
2016-02-27 19:46:17 +0100

    WIP stage screen. FFT audio analysis from live feed, key monitoring. needs to emit + receive data from sockt

--- assets/scripts/stage.js
+++ assets/scripts/stage.js
@@ -1,19 +1,41 @@
-var stage = (function(){
+var Stage = (function() {
 
-  //expose a global socket for client (this app)
-  var socket = io();
-  var data = {"color" : '#'+Math.floor(Math.random()*16777215).toString(16)};
-  
-  socket.emit('stage-connect', data);
+	//expose a global socket for client (this app)
+	var socket = io();
+	var data = {
+		"color": '#' + Math.floor(Math.random() * 16777215).toString(16)
+	};
 
-  var clickRedBtn = function(){
-    socket.emit('clicked-red-button-stage', data);
+	socket.emit('stage-connect', data);
+
+	var clickRedBtn = function() {
+		socket.emit('clicked-red-button-stage', data);
+	}
+
+  var keyCodePressed = '';
+
+  var keyPress = function(e){
+    keyCodePressed = e.code;
+    socket.emit('key-codes',{'keyCode': e.code});
+  };
+
+
+  var drawKeys = function(){
+    if (monitor==undefined) {
+        var monitor = document.getElementById('keysMonitor');
+    }
+
+    monitor.innerHTML = keyCodePressed;
+    window.requestAnimationFrame(drawKeys);
   }
 
-  //expose public vars and/or functions
-  return {
-    socket        : socket,
-    clickRedBtn   : clickRedBtn
-  };
+  window.addEventListener('keypress',keyPress);
+  window.addEventListener('keyup', function(){ keyCodePressed = '' });
+  drawKeys();
+	//expose public vars and/or functions
+	return {
+		socket: socket,
+		clickRedBtn: clickRedBtn
+	};
 
 })();
